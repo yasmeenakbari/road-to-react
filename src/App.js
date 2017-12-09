d89dcc56382256e73846caba2e40fbaac4573cba
Yasmeen Akbari <yasmeena@appnovation.com>
2017-12-09 09:52:23 -0800

    Adding style

--- src/App.js
+++ src/App.js
@@ -51,13 +51,15 @@ class App extends Component {
     const {searchTerm, list} = this.state;
 
     return (
-      <div className="App">
-        <Search 
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        >
-          Search
-        </Search>
+      <div className="page">
+        <div className="interactions">
+          <Search 
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+        </div>
 
         <Table
           list={list}
@@ -72,7 +74,7 @@ class App extends Component {
 
 const Search = ({value, onChange, children}) =>
   <form>
-    {children}
+    {children + " "}
     <input
       type="text"
       value={value}
@@ -82,17 +84,26 @@ const Search = ({value, onChange, children}) =>
 
 
 const Table = ({list, pattern, onDismiss}) =>
-  <div>
+  <div className="table">
     {list.filter(isSearched(pattern)).map(item =>
-    <div key={item.objectID}>
-      <span>
+    <div key={item.objectID} className="table-row">
+      <span style={{ width: '40%' }}>
         <a href={item.url}>{item.title}</a>
       </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
-        <Button onClick={() => onDismiss(item.objectID)}>
+      <span style={{ width: '30%' }}>
+        {item.author}
+      </span>
+      <span style={{ width: '10%' }}>
+        {item.num_comments}
+      </span>
+      <span style={{ width: '10%' }}>
+        {item.points}
+      </span>
+      <span style={{ width: '10%' }}>
+        <Button 
+          onClick={() => onDismiss(item.objectID)}
+          className="button-inline"
+        >
           Dismiss
         </Button>
       </span>
