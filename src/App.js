3c13868b2d09a90c244e9a9ff5f1cef673f8ddc7
Yasmeen Akbari <yasmeena@appnovation.com>
2017-12-11 20:19:09 -0800

    Added pagination

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 
 
@@ -27,7 +30,20 @@ class App extends Component {
   }
 
   setSearchTopStories(result){
-    this.setState({result});
+    const { hits, page } = result;
+
+    const oldHits = page !== 0
+      ? this.state.result.hits
+      : [];
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+
+    this.setState({
+      result: { hits: updatedHits, page }
+    });
   }
 
   onSearchSubmit(event) {
@@ -36,8 +52,10 @@ class App extends Component {
     event.preventDefault();
   }
 
-  fetchSearchTopStories(searchTerm){
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0){
+    console.log(page);
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}\
+${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(e => e);
@@ -62,8 +80,7 @@ class App extends Component {
 
   render() {
     const {searchTerm, result} = this.state;
-
-    if (!result) { return null; }
+    const page = (result && result.page) || 0;
 
     return (
       <div className="page">
@@ -84,6 +101,12 @@ class App extends Component {
           />
           : null
         }
+
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            More
+          </Button>
+        </div>
 
       </div>
     );
