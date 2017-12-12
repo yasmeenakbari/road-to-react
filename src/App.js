abcaaae8b0a7e60346d2ad89d644219f204d6198
Yasmeen Akbari <yasmeena@appnovation.com>
2017-12-11 19:51:46 -0800

    Replaced client-side search with server-side search

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 
 
-const isSearched = searchTerm => item => 
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
 
@@ -25,10 +23,17 @@ class App extends Component {
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
   }
 
   setSearchTopStories(result){
     this.setState({result});
+  }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
   }
 
   fetchSearchTopStories(searchTerm){
@@ -66,36 +71,41 @@ class App extends Component {
           <Search 
             value={searchTerm}
             onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
           >
             Search
           </Search>
         </div>
 
-        <Table
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { result
+          ? <Table
+            list={result.hits}
+            onDismiss={this.onDismiss}
+          />
+          : null
+        }
 
       </div>
     );
   }
 }
 
-const Search = ({value, onChange, children}) =>
-  <form>
-    {children + " "}
+const Search = ({value, onChange, onSubmit, children}) =>
+  <form onSubmit={onSubmit}>
     <input
       type="text"
       value={value}
       onChange={onChange}
     />
+    <button type="submit">
+      {children}
+    </button>
   </form>
 
 
-const Table = ({list, pattern, onDismiss}) =>
+const Table = ({list, onDismiss}) =>
   <div className="table">
-    {list.filter(isSearched(pattern)).map(item =>
+    {list.map(item =>
     <div key={item.objectID} className="table-row">
       <span style={{ width: '40%' }}>
         <a href={item.url}>{item.title}</a>
